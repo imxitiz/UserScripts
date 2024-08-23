6d30b34cc78849053f2b8e0b0c95f669bffb3128
imxitiz <69164340+imxitiz@users.noreply.github.com>
2024-08-23 19:17:41 +0545

    previous: v2.0 added - Aug 22, 2024
    Locking Chat List Visibility.
    
    --
    Signed-off-by: Kshitiz Sharma (imxitiz)   <69164340+imxitiz@users.noreply.github.com>

--- MessengerHideChatList.user.js
+++ MessengerHideChatList.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Messenger Hide Chat List
 // @namespace   imxitiz's-Script
-// @version     1.0
+// @version     2.0
 // @grant       none
 // @license     GNU GPLv3
 // @author      imxitiz
@@ -12,16 +12,27 @@
 // ==/UserScript==
 
 (function () {
-    "use strict";
+    ("use strict");
 
     let hasInitialized = false;
-    const hideThreshold = 20;
+    const hideThreshold = 20; // Threshold in pixels to determine if sidebar should be hidden
     let eventParent;
     let isResizing = false;
     let userDefinedFlexBasis =
         parseFloat(getLocalStorageItem("userDefinedFlexBasis")) || 30;
     let userResizedOnce =
         getSessionStorageItem("userResizedOnce") === "true" || false;
+    let active = parseInt(getSessionStorageItem("active")) || 0;
+    // active states:
+    // 0 - Normal behavior: sidebar hidden on hover
+    // 1 - Always visible
+    // 2 - Always hidden
+    let lockPosition = JSON.parse(getSessionStorageItem("lockPosition")) || {
+        x: 0,
+        y: 0,
+    };
+    const clickThreshold = 80; // Threshold in pixels for precise unlocking
+    let wrongLockedPlaceAttempt = 0;
 
     // Helper functions for storage
     function setSessionStorageItem(key, value) {
@@ -40,7 +51,46 @@
         return localStorage.getItem(key);
     }
 
-    // Update sidebar visibility
+    // Create and append the resize handle to the sidebar
+    function createResizeHandle() {
+        try {
+            const sidebar = document.querySelector(
+                'div[aria-label="Thread list"]'
+            );
+            if (!sidebar) {
+                throw new Error("Sidebar element not found");
+            }
+
+            // Create resize handle only if it doesn't already exist
+            if (!document.getElementById("resize-handle")) {
+                const resizeHandle = document.createElement("div");
+                resizeHandle.id = "resize-handle";
+                resizeHandle.style.width = "10px";
+                resizeHandle.style.height = "100%";
+                resizeHandle.style.position = "absolute";
+                resizeHandle.style.top = "0";
+                resizeHandle.style.right = "0";
+                resizeHandle.style.cursor = "ew-resize";
+                resizeHandle.style.backgroundColor = userResizedOnce
+                    ? "transparent"
+                    : "red";
+                resizeHandle.style.zIndex = "1000";
+                sidebar.appendChild(resizeHandle);
+
+                // Add event listeners for resizing
+                resizeHandle.addEventListener("mousedown", function () {
+                    isResizing = true;
+                    document.addEventListener("mousemove", resizeChatList);
+                    document.addEventListener("mouseup", stopResize);
+                });
+            }
+        } catch (error) {
+            console.error("Error creating resize handle: ", error);
+            setTimeout(createResizeHandle, 1000); // Retry after 1 second if there's an error
+        }
+    }
+
+    // Update sidebar visibility based on user interactions and settings
     function updateSidebarVisibility() {
         const sidebar = document.querySelector('div[aria-label="Thread list"]');
         const inboxSwitcher = document.querySelector(
@@ -54,49 +104,36 @@
                 sidebar.style.margin = "0";
                 hasInitialized = true;
 
-                setTimeout(() => {
-                    // Create and append the resize handle
-                    const resizeHandle = document.createElement("div");
-                    resizeHandle.id = "resize-handle";
-                    resizeHandle.style.width = "10px";
-                    resizeHandle.style.height = "100%";
-                    resizeHandle.style.position = "absolute";
-                    resizeHandle.style.top = "0";
-                    resizeHandle.style.right = "0";
-                    resizeHandle.style.cursor = "ew-resize";
-                    resizeHandle.style.backgroundColor = userResizedOnce
-                        ? "transparent"
-                        : "red";
-                    resizeHandle.style.zIndex = "1000";
-                    const sidebar = document.querySelector(
-                        'div[aria-label="Thread list"]'
-                    );
-                    sidebar.appendChild(resizeHandle);
-
-                    // Start resizing when the user clicks on the resize handle
-                    resizeHandle.addEventListener("mousedown", function () {
-                        isResizing = true;
-                        document.addEventListener("mousemove", resizeChatList);
-                        document.addEventListener("mouseup", stopResize);
-                    });
-                }, 5000);
+                createResizeHandle(); // Create resize handle on initial setup
             }
             const isMouseOverSidebar =
                 isMouseOver(sidebar) || isMouseOver(inboxSwitcher);
 
-            if (
-                isMouseOverSidebar ||
-                eventParent.clientX <= hideThreshold ||
-                isResizing
-            ) {
-                // Show sidebar
-                sidebar.style.maxWidth = `${userDefinedFlexBasis}%`; // Set max-width to its original value
-                sidebar.style.minWidth = `${userDefinedFlexBasis}%`; // Ensure min-width to maintain space
+            if (active === 1) {
+                // Always show the sidebar
+                sidebar.style.maxWidth = `${userDefinedFlexBasis}%`;
+                sidebar.style.minWidth = `${userDefinedFlexBasis}%`;
                 sidebar.style.margin = "0";
+            } else if (active === 2) {
+                // Always hide the sidebar
+                sidebar.style.maxWidth = "0";
+                sidebar.style.minWidth = "0";
             } else {
-                // Hide sidebar
-                sidebar.style.maxWidth = "0"; // Collapse max-width to zero
-                sidebar.style.minWidth = "0"; // Ensure min-width to collapse space
+                // Normal behavior
+                if (
+                    isMouseOverSidebar ||
+                    eventParent.clientX <= hideThreshold ||
+                    isResizing
+                ) {
+                    // Show sidebar
+                    sidebar.style.maxWidth = `${userDefinedFlexBasis}%`;
+                    sidebar.style.minWidth = `${userDefinedFlexBasis}%`;
+                    sidebar.style.margin = "0";
+                } else {
+                    // Hide sidebar
+                    sidebar.style.maxWidth = "0";
+                    sidebar.style.minWidth = "0";
+                }
             }
         }
     }
@@ -115,8 +152,8 @@
 
             if (newFlexBasis >= 10 && newFlexBasis <= 100) {
                 userDefinedFlexBasis = newFlexBasis;
-                sidebar.style.maxWidth = `${userDefinedFlexBasis}%`; // Set max-width to its original value
-                sidebar.style.minWidth = `${userDefinedFlexBasis}%`; // Ensure min-width to maintain space
+                sidebar.style.maxWidth = `${userDefinedFlexBasis}%`;
+                sidebar.style.minWidth = `${userDefinedFlexBasis}%`;
             }
 
             if (!userResizedOnce) {
@@ -124,10 +161,6 @@
                 setSessionStorageItem("userResizedOnce", "true");
                 let resizeHandle = document.getElementById("resize-handle");
                 resizeHandle.style.backgroundColor = "transparent";
-                // Optionally use a tooltip or status message instead of an alert
-                alert(
-                    "Resize handle color changed to transparent from red. You can now resize the chat list by dragging from the right edge of the chat list."
-                );
             }
             setLocalStorageItem("userDefinedFlexBasis", userDefinedFlexBasis);
         }
@@ -145,23 +178,130 @@
         return element.contains(eventParent.target);
     }
 
-    // Set up MutationObserver
-    const observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            if (mutation.type === "childList") {
-                // If there are added nodes, update the sidebar visibility
-                if (mutation.addedNodes.length > 0) {
-                    updateSidebarVisibility();
+    // Calculate distance between two points
+    function calculateDistance(pos1, pos2) {
+        return Math.sqrt(
+            Math.pow(pos1.x - pos2.x, 2) + Math.pow(pos1.y - pos2.y, 2)
+        );
+    }
+
+    // Check if mouse is near the locked position
+    function isNearLockPosition(x, y, lockPosition) {
+        const distance = calculateDistance(lockPosition, { x, y });
+        return distance <= clickThreshold;
+    }
+
+    // Helper function to show notifications
+    function showNotification(message, time = 5000) {
+        const notification = document.createElement("div");
+        notification.textContent = message;
+        notification.style.position = "fixed";
+        notification.style.top = "50%";
+        notification.style.left = "50%";
+        notification.style.transform = "translate(-50%, -50%)";
+        notification.style.padding = "10px";
+        notification.style.backgroundColor = "#333";
+        notification.style.color = "#fff";
+        notification.style.fontFamily = "Arial, sans-serif";
+        notification.style.fontSize = "20px";
+        notification.style.borderRadius = "5px";
+        notification.style.zIndex = "100000";
+        notification.style.opacity = "0";
+        notification.style.transition = "opacity 0.5s";
+        document.body.appendChild(notification);
+
+        // Fade in the notification
+        setTimeout(() => {
+            notification.style.opacity = "1";
+        }, 100);
+
+        // Fade out and remove the notification after 3 seconds
+        setTimeout(() => {
+            notification.style.opacity = "0";
+            setTimeout(() => {
+                document.body.removeChild(notification);
+            }, 500);
+        }, time);
+    }
+
+    // Triple click event listener
+    function handleTripleClick(event) {
+        if (event.detail === 3) {
+            const inboxSwitcher = document.querySelector(
+                'div[aria-label="Inbox switcher"]'
+            );
+            if (active === 0) {
+                if (isMouseOver(inboxSwitcher)) {
+                    active = 1;
+                    showNotification("Now chat list is always visible.", 3000);
+                } else {
+                    active = 2;
+                    lockPosition = { x: event.clientX, y: event.clientY };
+                    setSessionStorageItem(
+                        "lockPosition",
+                        JSON.stringify(lockPosition)
+                    );
+                    showNotification(
+                        "You have to triple click exactly here to unlock the chat list."
+                    );
+                }
+            } else if (active === 1) {
+                if (isMouseOver(inboxSwitcher)) {
+                    active = 0;
+                    showNotification("Now chat list is shown on hover.", 3000);
+                } else {
+                    active = 2;
+                    lockPosition = { x: event.clientX, y: event.clientY };
+                    setSessionStorageItem(
+                        "lockPosition",
+                        JSON.stringify(lockPosition)
+                    );
+                    showNotification(
+                        "You have to triple click exactly here to unlock the chat list."
+                    );
+                }
+            } else {
+                if (
+                    isNearLockPosition(
+                        event.clientX,
+                        event.clientY,
+                        lockPosition
+                    )
+                ) {
+                    active = 0;
+                    wrongLockedPlaceAttempt = 0;
+                    showNotification("Chat list unlocked successfully.");
+                } else {
+                    wrongLockedPlaceAttempt++;
+                    if (wrongLockedPlaceAttempt >= 10) {
+                        showNotification(
+                            "You can always logout and login again to reset the lock position."
+                        );
+                    } else {
+                        if (wrongLockedPlaceAttempt >= 5) {
+                            showNotification(
+                                "Please logout and login again to reset the lock position."
+                            );
+                        }
+                    }
+                    const clickPosition = {
+                        x: event.clientX,
+                        y: event.clientY,
+                    };
+                    const distance = calculateDistance(
+                        lockPosition,
+                        clickPosition
+                    );
+                    if (distance <= clickThreshold * 1.2) {
+                        showNotification(
+                            "You are near the locked position, try again!!!"
+                        );
+                    }
                 }
             }
-        });
-    });
-
-    // Start observing the document for changes
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-    });
+            setSessionStorageItem("active", active);
+        }
+    }
 
     // Initialize the script
     function init() {
@@ -169,6 +309,8 @@
             eventParent = event;
             updateSidebarVisibility();
         });
+
+        document.addEventListener("click", handleTripleClick);
     }
 
     init();
